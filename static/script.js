e54ebb861c4c82cfb34d90835ce39f3031db5d34
Maxi Palavecino <mpb71677@gmail.com>
2024-11-21 00:01:04 -0300

    Terminado sin base de datos

---
+++ static/script.js
@@ -0,0 +1,19 @@
+// Función para mostrar el modal
+function showPDF() {
+    document.getElementById("pdfModal").style.display = "block";
+}
+
+// Función para cerrar el modal
+function closePDF() {
+    document.getElementById("pdfModal").style.display = "none";
+}
+
+// Función para mostrar el modal de login
+function showLogin() {
+    document.getElementById("loginModal").style.display = "block";
+}
+
+// Función para cerrar el modal de login
+function closeLogin() {
+    document.getElementById("loginModal").style.display = "none";
+}
